3689345a152c064ca0fb7f7fc9430d0c06a1cc00
Jaafar MADIH <madih.jaafar@gmail.com>
2017-12-10 00:28:08 +0100

    [Step-02] set template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Introduction, Status} from './introduction';
-import {Link} from './link';
+import { Status} from './introduction';
 
 @Component({
   selector: 'app-home',
@@ -9,24 +8,11 @@ import {Link} from './link';
 })
 export class HomeComponent implements OnInit {
 
-  private introduction : Introduction;
-
   constructor() {
-    this.introduction = new Introduction();
   }
 
   ngOnInit() {
-    this.introduction.firstname = "Lucie";
-    this.introduction.lastname = "Macdonie"
-    this.introduction.birthDate = "10/09/1985";
-    this.introduction.email = "lucie.macdonie@gmail.com";
-    this.introduction.description = "I am a professional tiger wrestler. I wrestled the biggest Siberian Tigers for the opening act at the Awesome and Dangerous Circus.";
-    this.introduction.imageUrl = "assets/images/me.jpg";
-    this.introduction.status = Status.AVAILABLE;
-    this.introduction.links = [
-      new Link("", "./assets/images/linkedin.png"),
-      new Link("", "./assets/images/github.png"),
-      new Link("", "./assets/images/codepen.png")];
+
   }
 
   isAvailable(status: Status): boolean{
