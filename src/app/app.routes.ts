a669fe60556951aabb7fb7f0ab69b0a27783c6bf
Jaafar MADIH <madih.jaafar@gmail.com>
2017-12-15 17:36:02 +0100

    [Step-04-solution] Router #10 hide

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,3 @@
 import {Routes} from '@angular/router';
-import {NotFoundComponent} from './not-found/not-found.component';
-import {HomeComponent} from './home/home.component';
-import {SkillsComponent} from './skills/skills.component';
 
-export const appRoutes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'skills', component: SkillsComponent},
-  {path: '**', component: NotFoundComponent}
-];
+export const appRoutes: Routes = [];
