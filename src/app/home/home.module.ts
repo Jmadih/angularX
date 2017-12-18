7a7fc50f6c8f224db0fcbeb573eef7fd909ab4c3
Jaafar MADIH <madih.jaafar@gmail.com>
2017-12-18 14:07:14 +0100

    [Step-03] Directive #6 hide

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import {HomeComponent} from './home.component';
 import {SharedModule} from '../shared/shared.module';
-import { YearsOfExperienceDirective } from './years-of-experience.directive';
 
 @NgModule({
   imports: [
     SharedModule
   ],
-  declarations: [HomeComponent, YearsOfExperienceDirective],
+  declarations: [HomeComponent],
   exports: [HomeComponent]
 })
 export class HomeModule { }
--- src/app/home/years-of-experience.directive.spec.ts
+++
@@ -1,6 +0,0 @@
-import { YearsOfExperienceDirective } from './years-of-experience.directive';
-
-describe('YearsOfExperienceDirective', () => {
-  it('should create an instance', () => {
-  });
-});
