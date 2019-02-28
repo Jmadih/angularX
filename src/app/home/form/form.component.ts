c672e84c058d36519d45f7bf151f3bf6fc99038d
Jaafar MADIH <madih.jaafar@gmail.com>
2019-02-28 17:47:38 +0100

    minor fix

--- src/app/home/form/form.component.ts
+++ src/app/home/form/form.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit} from '@angular/core';
 import {FormGroup} from '@angular/forms';
 import {Introduction} from '../../shared/models/introduction';
 import {MatDialogRef} from '@angular/material';
