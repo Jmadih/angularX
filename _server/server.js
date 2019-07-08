10bb528a911b312b373020804745e8a2e30287f2
Jaafar MADIH <madih.jaafar@gmail.com>
2019-07-09 00:56:34 +0200

    server minor fix

--- _server/server.js
+++ _server/server.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 
-var express = require('express'),
-  bodyParser = require('body-parser'),
-  http = require('http'),
-  path = require('path'),
-  api = require('./routes/api'),
-  cors = require('cors');
+var express = require("express");
+var bodyParser = require("body-parser");
+var http = require("http");
+var path = require("path");
+var api = require("./routes/api");
+var cors = require("cors");
 
 var app = express();
 
-app.set('port', process.env.PORT || 9000);
+app.set("port", process.env.PORT || 9000);
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -26,20 +26,20 @@ app.use(function(req, res, next) {
 /**
  * Introductions
  */
-app.get('/api/intros', api.getIntros);
-app.get('/api/intros/:id', api.getIntro);
-app.put('/api/intros/:id', api.updateIntro);
+app.get("/api/intros", api.getIntros);
+app.get("/api/intros/:id", api.getIntro);
+app.put("/api/intros/:id", api.updateIntro);
 
 
 /**
  *  Skills
  */
-app.get('/api/skills', api.getSkills);
-app.put('/api/skills/:id', api.updateSkill);
-app.post('/api/skills', api.addSkill);
-app.delete('/api/skills/:id', api.deleteSkill);
+app.get("/api/skills", api.getSkills);
+app.put("/api/skills/:id", api.updateSkill);
+app.post("/api/skills", api.addSkill);
+app.delete("/api/skills/:id", api.deleteSkill);
 
 
-app.listen(app.get('port'), function () {
-  console.log('✔Express server listening on http://localhost:%d/', app.get('port'));
+app.listen(app.get("port"), function() {
+  console.log("✔Express server listening on http://localhost:%d/", app.get("port"));
 });
