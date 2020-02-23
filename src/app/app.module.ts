82f1e7568b7e754484b0b1890b303faed90e4421
Jaafar MADIH <madih.jaafar@gmail.com>
2020-02-23 20:58:59 +0100

    fix some warnings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {SharedModule} from './shared/shared.module';
 
-
-import { AppComponent } from './app.component';
-//import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
-    //,HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
