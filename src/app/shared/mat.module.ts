85e781b1901ad148f7010862eeaf42d89123dc58
Jaafar MADIH <madih.jaafar@gmail.com>
2020-02-23 20:58:59 +0100

    fix some warnings

--- src/app/shared/mat.module.ts
+++ src/app/shared/mat.module.ts
@@ -1,30 +1,25 @@
 import {NgModule} from '@angular/core';
-import {
-  MatAutocompleteModule,
-  MatButtonModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatFormFieldModule,
-  MatIconModule,
-  MatInputModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatOptionModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatSelectModule,
-  MatSliderModule,
-  MatSnackBarModule,
-  MatToolbarModule,
-  MatTooltipModule
-} from '@angular/material';
+
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatOptionModule} from '@angular/material/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatCardModule} from '@angular/material/card';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
 
 
-export let Mat_MODULES: any = [
+export let MatMODULES: any = [
   BrowserAnimationsModule,
   MatButtonModule,
   MatCardModule,
@@ -45,7 +40,7 @@ export let Mat_MODULES: any = [
 ];
 
 @NgModule({
-  exports: [...Mat_MODULES],
+  exports: [...MatMODULES],
 })
 export class MatModule {
 }
